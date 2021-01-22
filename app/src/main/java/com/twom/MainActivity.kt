d4f200c308d40abbcaef822a3e4a4c9d65dad666
hkmonumeena <monum811@gamail.com>
2021-01-22 14:14:27 +0530

    my msg

--- app/src/main/java/com/twom/MainActivity.kt
+++ app/src/main/java/com/twom/MainActivity.kt
@@ -2,10 +2,19 @@ package com.twom
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val list = listOf("category_id" to "2","subcategory_id" to "2")
+        TwoM.post("https://maestrosinfotech.com/rushabhbusinesscentre/api/process.php?action=show_product")
+        TwoM.bodyParameterList(list)
+        TwoM.PostExecute(this).get{
+            result, error ->
+            Log.e("dmsjgh", "result: $result")
+            Log.e("dmsjgh", "error: $error")
+        }
     }
 }
--- app/src/main/java/com/twom/TwoM.kt
+++ app/src/main/java/com/twom/TwoM.kt
@@ -44,86 +44,9 @@ object TwoM {
         return urlmain!!
     }
 
-    class GetExecute(context: Context) {
-        private   val url: String = urlmain!!
-        private val context = context
-        private fun isNetworkAvailable(): Boolean? {
-            val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE)
-            return if (connectivityManager is ConnectivityManager) {
-                val networkInfo = connectivityManager.activeNetworkInfo
-                networkInfo?.isConnected
-            }
-
-            else false
-
-        }
-
-
-
-
-        fun get(myCallback: (result: String?, error: String?) -> Unit) {
-
-            var httpURlConnection = URL(url).openConnection() as HttpURLConnection
-
-
-            if (isNetworkAvailable()==true){
-                GlobalScope.launch(Dispatchers.IO) {
-                    httpURlConnection.requestMethod = "GET"
-                    httpURlConnection.readTimeout = 60000
-                    httpURlConnection.connectTimeout = 60000
-                    httpURlConnection.setRequestProperty("Connection", "Keep-Alive");
-                    httpURlConnection.setRequestProperty("Cache-Control", "no-cache");
-                    httpURlConnection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded")
-                    for ((key, value) in headerMap!!.entries) {
-                        httpURlConnection.setRequestProperty(key, value)
-                    }
-                    httpURlConnection.doInput = true
-                    httpURlConnection.doOutput = true
-                    val os: OutputStream = httpURlConnection.outputStream
-                    val writer = BufferedWriter(OutputStreamWriter(os, "UTF-8"))
-                    writer.write(encodeParams(bodyparams))
-                    writer.flush()
-                    writer.close()
-                    os.close()
-                    httpURlConnection.connect()
-                    val data = httpURlConnection.inputStream.bufferedReader().readText()
-                    httpURlConnection.disconnect()
-
-                    GlobalScope.launch(Dispatchers.Main){
-                        if (httpURlConnection.responseCode == 200) {
-                            myCallback.invoke(data,null)
-
-                        }
-                        else {
-                            //  throw IOException("Server returned non-OK status: ${httpURlConnection.responseCode}")
-
-                            myCallback.invoke(null, "Error to connect with this url check all the fields -  ${httpURlConnection.responseCode}")
-
-                        }
-
-
-
-                    }
-
-
-                }
-
-            }
-
-            else {
-                try {
-                    myCallback.invoke(null,"Server returned non-OK status ${httpURlConnection.responseCode}: Not Internet")
-                } catch (e: Exception) {
-                    myCallback.invoke(null,"${e}: Not Internet")
-                }
-
-            }
-
-        }
-    }
     class PostExecute(context: Context) {
-        private   val url: String = urlmain!!
-        private val context = context
+     private   val url: String = urlmain!!
+private val context = context
         private fun isNetworkAvailable(): Boolean? {
             val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE)
             return if (connectivityManager is ConnectivityManager) {
@@ -172,7 +95,7 @@ object TwoM {
 
                         }
                         else {
-                            //  throw IOException("Server returned non-OK status: ${httpURlConnection.responseCode}")
+                          //  throw IOException("Server returned non-OK status: ${httpURlConnection.responseCode}")
 
                             myCallback.invoke(null, "Error to connect with this url check all the fields -  ${httpURlConnection.responseCode}")
 
@@ -192,6 +115,46 @@ object TwoM {
                     myCallback.invoke(null,"Server returned non-OK status ${httpURlConnection.responseCode}: Not Internet")
                 } catch (e: Exception) {
                     myCallback.invoke(null,"${e}: Not Internet")
+                }
+
+            }
+
+        }
+    }
+    class GetExecute() {
+        val url: String = urlmain!!
+
+        fun get(myCallback: (result: String?, error: String?) -> Unit) {
+            GlobalScope.launch(Dispatchers.IO) {
+                val httpURlConnection = URL(url).openConnection() as HttpURLConnection
+                httpURlConnection.requestMethod = "GET"
+                httpURlConnection.readTimeout = 60000
+                httpURlConnection.connectTimeout = 60000
+                httpURlConnection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded")
+                for ((key, value) in headerMap!!.entries) {
+                    httpURlConnection.setRequestProperty(key, value)
+                }
+                httpURlConnection.doInput = true
+                httpURlConnection.doOutput = true
+                val os: OutputStream = httpURlConnection.outputStream
+                val writer = BufferedWriter(OutputStreamWriter(os, "UTF-8"))
+                writer.write(encodeParams(bodyparams))
+                writer.flush()
+                writer.close()
+                os.close()
+                httpURlConnection.connect()
+                val data = httpURlConnection.inputStream.bufferedReader().readText()
+                httpURlConnection.disconnect()
+                if (httpURlConnection.responseCode == 200) {
+                   GlobalScope.launch(Dispatchers.Main) {
+                       myCallback.invoke(data, null)
+                   }
+                }
+
+                else {
+                    GlobalScope.launch(Dispatchers.Main) {
+                        myCallback.invoke(null, "Error to connect with this url check all the fields")
+                    }
                 }
 
             }
